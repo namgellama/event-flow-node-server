d75d6ec0b68b2fc4fac5bc34633538839ae84948
namgel <namgeltamang14@gmail.com>
2026-08-05 10:08:38 +0545

    Seed admin

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,22 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import { env } from "../config/env";
+import { usersTable } from "./schema";
+import bcrypt from "bcryptjs";
 
 const pool = new Pool({ connectionString: env.DATABASE_URL });
 
 const db = drizzle({ client: pool });
+
+async function main() {
+    const user: typeof usersTable.$inferInsert = {
+        name: "Admin",
+        email: "admin@admin.com",
+        password: bcrypt.hashSync("admin"),
+        role: "ADMIN",
+    };
+    await db.insert(usersTable).values(user);
+    console.log("Admin user created!");
+}
+
+main();
