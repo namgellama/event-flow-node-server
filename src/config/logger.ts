9b242759758da7d409e1f94a42285dd73b911e66
namgel <namgeltamang14@gmail.com>
2026-09-02 14:33:43 +0545

    Configure pino-pretty

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -3,4 +3,14 @@ import { env } from "./env";
 
 export const logger = pino({
     level: env.LOG_LEVEL,
+    ...(process.env.NODE_ENV === "development" && {
+        transport: {
+            target: "pino-pretty",
+            options: {
+                colorize: true,
+                translateTime: "SYS:standard",
+                ignore: "pid,hostname",
+            },
+        },
+    }),
 });
